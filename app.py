a8c1e3fe70c81fae452dc6f7746d29ca73d55123
JeongUngKim <120348508+JeongUngKim@users.noreply.github.com>
2022-12-19 16:20:57 +0900

    2022-12-19 중간

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ import streamlit as st
 from home import home
 from team import team
 from player import player
+import matplotlib.pyplot as plt
+import platform
+from matplotlib import font_manager, rc
+plt.rcParams['axes.unicode_minus'] = False
+if platform.system() == 'Linux':
+    rc('font', family='NanumGothic')
+
 def main() : 
-    team_name_list = ['','ATL', 'BOS', 'NOP', 'CHI', 'DAL', 'DEN', 'HOU', 'LAC', 'LAL',
+
+
+
+    team_name_list = ['(팀을 선택해주세요)','ATL', 'BOS', 'NOP', 'CHI', 'DAL', 'DEN', 'HOU', 'LAC', 'LAL',
        'MIA', 'MIL', 'MIN', 'BKN', 'NYK', 'ORL', 'IND', 'PHI', 'PHX',
        'POR', 'SAC', 'SAS', 'OKC', 'TOR', 'UTA', 'MEM', 'WAS', 'DET',
        'CHA', 'CLE', 'GSW']
@@ -11,7 +21,7 @@ def main() :
     
     team_name = st.sidebar.selectbox('team',team_name_list)
     player_name = st.sidebar.selectbox('player info',[''])
-    if team_name != '' :
+    if team_name != '(팀을 선택해주세요)' :
         team(team_name)
     elif player_name != '' :
         player(player_name)
