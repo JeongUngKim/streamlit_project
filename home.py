eddf014e55170d0353a8b2258913dee9fe6d35c4
JeongUngKim <120348508+JeongUngKim@users.noreply.github.com>
2022-12-23 11:03:57 +0900

    2022-12-23 에러확인용

--- home.py
+++ home.py
@@ -5,6 +5,8 @@ def home() :
     image_url = 'https://mblogthumb-phinf.pstatic.net/MjAxOTA2MTNfMjkw/MDAxNTYwMzkyODk3MDQ0._UQzxFUpS-lLFTg5FX4nsP6o9WxKBVB1IlEAoo3Scz8g.50FBo4vY3wwoXSPk2-Pzt5mP_mM_gx8n2HsbZ7ZR7Lkg.PNG.swmh69/nba_%EB%A1%9C%EA%B3%A0.png?type=w800'
     st.image(image_url)
     st.subheader('')
+
+    st.title('')
     
 
     
