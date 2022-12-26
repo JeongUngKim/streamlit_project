a738dfbabb36ab5e885c6f0297fec707cddd69f3
JeongUngKim <120348508+JeongUngKim@users.noreply.github.com>
2022-12-26 12:09:02 +0900

    2022-12-26 푸티테스트

--- home.py
+++ home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import data_init
 def home() :
     st.title('NBA 데이터 센터에 오신걸 환영합니다.')
+    st.subheader('푸티테스트')
     image_url = 'https://mblogthumb-phinf.pstatic.net/MjAxOTA2MTNfMjkw/MDAxNTYwMzkyODk3MDQ0._UQzxFUpS-lLFTg5FX4nsP6o9WxKBVB1IlEAoo3Scz8g.50FBo4vY3wwoXSPk2-Pzt5mP_mM_gx8n2HsbZ7ZR7Lkg.PNG.swmh69/nba_%EB%A1%9C%EA%B3%A0.png?type=w800'
     st.image(image_url)
     st.subheader('')
